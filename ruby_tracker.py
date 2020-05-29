21c5d2a0b6bf0fad7cdce9cf7142cb82b262406b
rpbaskin <rpbaskin@gmail.com>
2020-05-29 13:26:29 -0600

    Added information about sources and made code simpler by removing some

--- ruby_tracker.py
+++ ruby_tracker.py
@@ -1,10 +1,6 @@
-#######################################################
-#						RUBY TRACKER                  #
-#       A script to track objects in a video          #
-#######################################################
-# Paul Baskin
-# University of Utah 
-
+# Adapted for research use from Adrian Rosebrock, OpenCV Object Track Object Movement, PyImageSearch,
+# https://www.pyimagesearch.com/2015/09/21/opencv-track-object-movement/
+# by Robert Paul Baskin at the Univeristy of Utah
 
 # USAGE
 # python videoflow.py --video video_to_scan.mp4
@@ -25,7 +21,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-v", "--video",
                 help="path to the video file", required=True)
 parser.add_argument("-t", "--tail", type=int, default=64,
-                help="max tail size")
+                help="tail length")
 parser.add_argument("-s", "--show", help="Shows the frame being processed currently, slows down processing",
                 action='store_true')
 args = vars(parser.parse_args())
@@ -35,19 +31,17 @@ args = vars(parser.parse_args())
 pts = deque(maxlen=args["tail"])
 out = []
 counter = 0
-(dX, dY) = (0, 0)
 
-# define the boundaries of the "white"
-sens = 15
-whiteLow = np.array([0, 0, 255 - sens])
-whiteUp = np.array([255, sens, 255])
+# define the boundaries of the white color
+sens = 15 
+#whiteLow = np.array([0, 0, 255 - sens])
+#whiteUp = np.array([255, sens, 255])
 
-#for color
-#whiteLow = np.array([0, 0, 30])
-#whiteUp = np.array([360, 100, 100])
+whiteLow = np.array([0, 0, 30])
+whiteUp = np.array([360, 100, 100])
 
 
-#if a video path was not found, grab the webcam otherwise hit that video
+#grabs the video to begin processing
 if not args.get("video", False):
     vs = VideoStream(src=0).start()
     outputfile = open("camera_video.csv", 'w')
@@ -60,30 +54,28 @@ fps = vs.get(5)
 totalframes = vs.get(7)
 print(str(totalframes) + " total frames to be processed.")
 
-time.sleep(1.0)
-
 framestate = 0
 while True:
     # current frame
     frame = vs.read()
 
-    # handle the frame from VideoCapture or VideoStream
+    # handle the frame from VideoCapture
     frame = frame[1] if args.get("video", False) else frame
 
-    # if we are viewing a video and we did not grab a frame,
-    # then we have reached the end of the video
     if frame is None:
         break
 
-    # resize the frame, blur it, and convert it to the HSV
-    # color space #for white just do 11
+    # resize the frame, blur it, and convert it to HSV, this
+    # allows us to input the color desired to be tracked (white)
+    # in a range of colors.
+
+    # essentially grabs a layer which contains all pixels within
+    # the color range. Then scrubs the noise away
+    
     frame = imutils.resize(frame, width=600)
     blurred = cv2.GaussianBlur(frame, (19, 19), 0)
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-
-    # construct a mask for the color "white", then perform
-    # a series of dilations and erosions to remove any small
-    # blobs left in the mask
+    
     mask = cv2.inRange(hsv, whiteLow, whiteUp)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
@@ -97,18 +89,19 @@ while True:
 
     # only proceed if at least one contour was found
     if len(counts) > 0:
+        
         # find the largest contour in the mask, then use
         # it to compute the minimum enclosing circle and
-        # centroid
+        # this permits us to identify and follow the fish
+        
         c = max(counts, key=cv2.contourArea)
         ((x, y), radius) = cv2.minEnclosingCircle(c)
         M = cv2.moments(c)
         center = int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"])
         out.append(str(int(M["m10"] / M["m00"])) + ',' + str(int(M["m01"] / M["m00"])) + '\n')
 
-        # only proceed if the radius (accuracy) meets a minimum size
-        if radius > .01:
-            pts.appendleft(center)
+
+        pts.appendleft(center)
 
     # loop over the set of tracked points
     for i in np.arange(1, len(pts)):
@@ -117,20 +110,12 @@ while True:
         if pts[i - 1] is None or pts[i] is None:
             continue
 
-        # make sure the tail is the right length
-        if counter >= 10 and i == 1 and pts[0] is not None:
-            # compute the difference between the x and y
-            # coordinates and re-initialize the direction
-            # text variables
-            dX = pts[0][0] - pts[i][0]
-            dY = pts[0][1] - pts[i][1]
-
-        # otherwise, compute the thickness of the line and
+        # compute the thickness of the line and
         # draw the connecting lines
-        thickness = int(np.sqrt(args["tail"] / float(i + 1)) * 2.5)
+        thickness = int(np.sqrt(args["tail"] / float(i + 1)) * 2)
         cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
 
-    # show the frame to our screen and increment the frame counter
+    # shows the processing to visually verify that it is working. 
     if args.get("show"):
         cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
@@ -151,7 +136,7 @@ if not args.get("video", False):
         i += 1
     vs.stop()
 
-# write me some output
+# write output
 else:
     outputfile.write('Frame,Xpos,Ypos,Velocity,\n')
     i = 1
